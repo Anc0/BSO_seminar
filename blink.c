9a0af69847120e754fc86727e4ef35ad125dd4e3
Jakob Salej <jakob.salej@gmail.com>
2018-05-27 17:23:43 +0200

    Trying to connect to Google IoT Core

--- blink.c
+++ blink.c
@@ -1,3 +1,33 @@
+/******************************************************************************
+ * Copyright 2017 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+// [START iot_mqtt_include]
+#define _XOPEN_SOURCE 500
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "jwt.h"
+#include "openssl/ec.h"
+#include "openssl/evp.h"
+#include "MQTTClient.h"
+// [END iot_mqtt_include]
+
 #include "espressif/esp_common.h"
 #include "esp/uart.h"
 #include "FreeRTOS.h"
@@ -29,113 +59,294 @@
 
 #define gpio_wemos_led	2
 
-typedef enum {
-	BMP280_TEMPERATURE, BMP280_PRESSURE
-} bmp280_quantity;
+#define TRACE 1 /* Set to 1 to enable tracing */
 
-typedef enum {
-	MPU9250_ACCEL_X = 0x3b,
-	MPU9250_ACCEL_Y = 0x3d,
-	MPU9250_ACCEL_Z = 0x3f,
-	MPU9250_TEMP = 0x41,
-	MPU9250_GYRO_X = 0x43,
-	MPU9250_GYRO_Y = 0x45,
-	MPU9250_GYRO_Z = 0x47
-} mpu9250_quantity;
-
-enum {
-	SSI_UPTIME, SSI_FREE_HEAP, SSI_LED_STATE
+struct {
+  char* address;
+  enum { clientid_maxlen = 256, clientid_size };
+  char clientid[clientid_size];
+  char* deviceid;
+  char* ecpath;
+  char* projectid;
+  char* region;
+  char* registryid;
+  char* rootpath;
+  char* topic;
+  char* payload;
+} opts = {
+  .address = "ssl://mqtt.googleapis.com:8883",
+  .clientid = "projects/{your-project-id}/locations/{your-region-id}/registries/{your-registry-id}/devices/{your-device-id}",
+  .deviceid = "{your-device-id}",
+  .ecpath = "ec_private.pem",
+  .projectid = "intense-wavelet-343",
+  .region = "{your-region-id}",
+  .registryid = "{your-registry-id}",
+  .rootpath = "roots.pem",
+  .topic = "/devices/{your-device-id}/events",
+  .payload = "Hello world!"
 };
 
-bmp280_t bmp280_dev;
-
-// read BMP280 sensor values
-float read_bmp280(bmp280_quantity quantity) {
-
-	float temperature, pressure;
-
-	bmp280_force_measurement(&bmp280_dev);
-	// wait for measurement to complete
-	while (bmp280_is_measuring(&bmp280_dev)) {
-	};
-	bmp280_read_float(&bmp280_dev, &temperature, &pressure, NULL);
-
-	if (quantity == BMP280_TEMPERATURE) {
-		return temperature;
-	} else if (quantity == BMP280_PRESSURE) {
-		return pressure;
-	}
-	return 0;
+void Usage() {
+  printf("mqtt_ciotc <message> \\\n");
+  printf("\t--deviceid <your device id>\\\n");
+  printf("\t--region <e.g. us-central1>\\\n");
+  printf("\t--registryid <your registry id>\\\n");
+  printf("\t--projectid <your project id>\\\n");
+  printf("\t--ecpath <e.g. ./ec_private.pem>\\\n");
+  printf("\t--rootpath <e.g. ./roots.pem>\n\n");
 }
 
-// write byte to PCF on I2C bus
-void write_byte_pcf(uint8_t data) {
-	i2c_slave_write(BUS_I2C, PCF_ADDRESS, NULL, &data, 1);
+// [START iot_mqtt_jwt]
+/**
+ * Calculates issued at / expiration times for JWT and places the time, as a
+ * Unix timestamp, in the strings passed to the function. The time_size
+ * parameter specifies the length of the string allocated for both iat and exp.
+ */
+static void GetIatExp(char* iat, char* exp, int time_size) {
+  // TODO: Use time.google.com for iat
+  time_t now_seconds = time(NULL);
+  snprintf(iat, time_size, "%lu", now_seconds);
+  snprintf(exp, time_size, "%lu", now_seconds + 3600);
+  if (TRACE) {
+    printf("IAT: %s\n", iat);
+    printf("EXP: %s\n", exp);
+  }
 }
 
-// read byte from PCF on I2C bus
-uint8_t read_byte_pcf() {
-	uint8_t data;
+/**
+ * Calculates a Java Web Token (JWT) given the path to a EC private key and
+ * Google Cloud project ID. Returns the JWT as a string that the caller must
+ * free.
+ */
+static char* CreateJwt(const char* ec_private_path, const char* project_id) {
+  char iat_time[sizeof(time_t) * 3 + 2];
+  char exp_time[sizeof(time_t) * 3 + 2];
+  uint8_t* key = NULL; // Stores the Base64 encoded certificate
+  size_t key_len = 0;
+  jwt_t *jwt = NULL;
+  int ret = 0;
+  char *out = NULL;
 
-	i2c_slave_read(BUS_I2C, PCF_ADDRESS, NULL, &data, 1);
-	return data;
+  // Read private key from file
+  FILE *fp = fopen(ec_private_path, "r");
+  if (fp == (void*) NULL) {
+    printf("Could not open file: %s\n", ec_private_path);
+    return "";
+  }
+  fseek(fp, 0L, SEEK_END);
+  key_len = ftell(fp);
+  fseek(fp, 0L, SEEK_SET);
+  key = malloc(sizeof(uint8_t) * (key_len + 1)); // certificate length + \0
+
+  fread(key, 1, key_len, fp);
+  key[key_len] = '\0';
+  fclose(fp);
+
+  // Get JWT parts
+  GetIatExp(iat_time, exp_time, sizeof(iat_time));
+
+  jwt_new(&jwt);
+
+  // Write JWT
+  ret = jwt_add_grant(jwt, "iat", iat_time);
+  if (ret) {
+    printf("Error setting issue timestamp: %d", ret);
+  }
+  ret = jwt_add_grant(jwt, "exp", exp_time);
+  if (ret) {
+    printf("Error setting expiration: %d", ret);
+  }
+  ret = jwt_add_grant(jwt, "aud", project_id);
+  if (ret) {
+    printf("Error adding audience: %d", ret);
+  }
+  ret = jwt_set_alg(jwt, JWT_ALG_ES256, key, key_len);
+  if (ret) {
+    printf("Error during set alg: %d", ret);
+  }
+  out = jwt_encode_str(jwt);
+
+  // Print JWT
+  if (TRACE) {
+    printf("JWT: [%s]", out);
+  }
+
+  jwt_free(jwt);
+  free(key);
+  return out;
 }
+// [END iot_mqtt_jwt]
 
-// check for pressed buttons
-void pcf_task(void *pvParameters) {
+/**
+ * Helper to parse arguments passed to app. Returns false if there are missing
+ * or invalid arguments; otherwise, returns true indicating the caller should
+ * free the calculated client ID placed on the opts structure.
+ *
+ * TODO: (class) Consider getopt
+ */
+// [START iot_mqtt_opts]
+bool GetOpts(int argc, char** argv) {
+  int pos = 1;
+  bool calcvalues = false;
 
-	uint8_t pcf_byte;
+  if (argc < 2) {
+    return false;
+  }
 
-	// turn off all leds
-	write_byte_pcf(leds_off);
+  opts.payload = argv[1];
 
-	while (1) {
-		printf("GOVNO");
-		pcf_byte = read_byte_pcf();
+  while (pos < argc) {
+    if (strcmp(argv[pos], "--deviceid") == 0) {
+      if (++pos < argc) {
+        opts.deviceid = argv[pos];
+        calcvalues = true;
+      }
+      else
+        return false;
+    } else if (strcmp(argv[pos], "--region") == 0) {
+      if (++pos < argc) {
+        opts.region = argv[pos];
+        calcvalues = true;
+      }
+      else
+        return false;
+    } else if (strcmp(argv[pos], "--registryid") == 0) {
+      if (++pos < argc) {
+        opts.registryid = argv[pos];
+        calcvalues = true;
+      }
+      else
+        return false;
+    } else if (strcmp(argv[pos], "--projectid") == 0) {
+      if (++pos < argc) {
+        opts.projectid = argv[pos];
+        calcvalues = true;
+      }
+      else
+        return false;
+    } else if (strcmp(argv[pos], "--ecpath") == 0) {
+      if (++pos < argc)
+        opts.ecpath = argv[pos];
+      else
+        return false;
+    } else if (strcmp(argv[pos], "--rootpath") == 0) {
+      if (++pos < argc)
+        opts.rootpath = argv[pos];
+      else
+        return false;
+    }
+    pos++;
+  }
 
-		// clear buttons states and toggle led 1
-		write_byte_pcf((pcf_byte ^ ~led1) | clr_btn);
-		// display temperature
-		printf("Temperature: %.2f C\n", read_bmp280(BMP280_TEMPERATURE));
+  if (calcvalues) {
+    int n = snprintf(opts.clientid, sizeof(opts.clientid),
+        "projects/%s/locations/%s/registries/%s/devices/%s",
+        opts.projectid, opts.region, opts.registryid, opts.deviceid);
+    if (n < 0 || (n > clientid_maxlen)) {
+      if (n < 0) {
+        printf("Encoding error!\n");
+      } else {
+        printf("Error, buffer for storing client ID was too small.\n");
+      }
+      return false;
+    }
+    if (TRACE) {
+      printf("New client id constructed:\n");
+      printf("%s\n", opts.clientid);
+    }
 
-
-		// check again after 200 ms
-		vTaskDelay(pdMS_TO_TICKS(1000));
-	}
+    return true; // Caller must free opts.clientid
+  }
+  return false;
 }
+// [END iot_mqtt_opts]
 
-void user_init(void) {
-	printf("Hello world");
+static const int kQos = 1;
+static const unsigned long kTimeout = 10000L;
+static const char* kUsername = "unused";
 
-	uart_set_baud(0, 115200);
-	i2c_init(BUS_I2C, SCL, SDA, I2C_FREQ_100K);
-	// fix i2c driver to work with MPU-9250
-	gpio_enable(SCL, GPIO_OUTPUT);
+static const unsigned long kInitialConnectIntervalMillis = 500L;
+static const unsigned long kMaxConnectIntervalMillis = 6000L;
+static const unsigned long kMaxConnectRetryTimeElapsedMillis = 900000L;
+static const float kIntervalMultiplier = 1.5f;
 
-	// WiFi configuration
-	//struct sdk_station_config config = { .ssid = WIFI_SSID, .password = WIFI_PASS, };
-	//sdk_wifi_station_set_auto_connect(1);
-	//sdk_wifi_set_opmode(STATION_MODE);
-	//sdk_wifi_station_set_config(&config);
-	//sdk_wifi_station_connect();
+/**
+ * Publish a given message, passed in as payload, to Cloud IoT Core using the
+ * values passed to the sample, stored in the global opts structure. Returns
+ * the result code from the MQTT client.
+ */
+// [START iot_mqtt_publish]
+int Publish(char* payload, int payload_size) {
+  int rc = -1;
+  MQTTClient client = {0};
+  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+  MQTTClient_message pubmsg = MQTTClient_message_initializer;
+  MQTTClient_deliveryToken token = {0};
 
-	// OTA configuration
-	//ota_tftp_init_server(TFTP_PORT);
+  MQTTClient_create(&client, opts.address, opts.clientid,
+      MQTTCLIENT_PERSISTENCE_NONE, NULL);
+  conn_opts.keepAliveInterval = 60;
+  conn_opts.cleansession = 1;
+  conn_opts.username = kUsername;
+  conn_opts.password = CreateJwt(opts.ecpath, opts.projectid);
+  MQTTClient_SSLOptions sslopts = MQTTClient_SSLOptions_initializer;
 
-	// BMP280 configuration
-	bmp280_params_t params;
-	bmp280_init_default_params(&params);
-	params.mode = BMP280_MODE_FORCED;
-	bmp280_dev.i2c_dev.bus = BUS_I2C;
-	bmp280_dev.i2c_dev.addr = BMP280_I2C_ADDRESS_0;
-	bmp280_init(&bmp280_dev, &params);
+  sslopts.trustStore = opts.rootpath;
+  sslopts.privateKey = opts.ecpath;
+  conn_opts.ssl = &sslopts;
 
-	// turn off Wemos led
-	gpio_enable(gpio_wemos_led, GPIO_OUTPUT);
-	gpio_write(gpio_wemos_led, 1);
+  unsigned long retry_interval_ms = kInitialConnectIntervalMillis;
+  unsigned long total_retry_time_ms = 0;
+  while ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+    if (rc == 3) {  // connection refused: server unavailable
+      usleep(retry_interval_ms / 1000);
+      total_retry_time_ms += retry_interval_ms;
+      if (total_retry_time_ms >= kMaxConnectRetryTimeElapsedMillis) {
+        printf("Failed to connect, maximum retry time exceeded.");
+        exit(EXIT_FAILURE);
+      }
+      retry_interval_ms *= kIntervalMultiplier;
+      if (retry_interval_ms > kMaxConnectIntervalMillis) {
+        retry_interval_ms = kMaxConnectIntervalMillis;
+      }
+    } else {
+      printf("Failed to connect, return code %d\n", rc);
+      exit(EXIT_FAILURE);
+    }
+  }
 
-	// create pcf task
-	xTaskCreate(pcf_task, "PCF task", 1000, NULL, 2, NULL);
+  pubmsg.payload = payload;
+  pubmsg.payloadlen = payload_size;
+  pubmsg.qos = kQos;
+  pubmsg.retained = 0;
+  MQTTClient_publishMessage(client, opts.topic, &pubmsg, &token);
+  printf("Waiting for up to %lu seconds for publication of %s\n"
+          "on topic %s for client with ClientID: %s\n",
+          (kTimeout/1000), opts.payload, opts.topic, opts.clientid);
+  rc = MQTTClient_waitForCompletion(client, token, kTimeout);
+  printf("Message with delivery token %d delivered\n", token);
+  MQTTClient_disconnect(client, 10000);
+  MQTTClient_destroy(&client);
 
+  return rc;
 }
+// [END iot_mqtt_publish]
 
+/**
+ * Connects MQTT client and transmits payload.
+ */
+// [START iot_mqtt_run]
+int main(int argc, char* argv[]) {
+  OpenSSL_add_all_algorithms();
+  OpenSSL_add_all_digests();
+  OpenSSL_add_all_ciphers();
+
+  if (GetOpts(argc, argv)) {
+    Publish(opts.payload, strlen(opts.payload));
+  } else {
+    Usage();
+  }
+
+  EVP_cleanup();
+}
+// [END iot_mqtt_run]
